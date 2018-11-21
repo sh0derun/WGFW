d5d4b717da207ae7d5139c844d303eade3eae6f3
RAZIK Anass <razik.anas@gmail.com>
2018-11-21 23:08:06 +0000

    first commit

---
+++ wgl.js
@@ -0,0 +1,25 @@
+class WGL{
+    constructor(selector){
+        this.canvas = document.querySelector(selector);
+        this.ctx = this.canvas.getContext("webgl");
+        if(!this.ctx){console.error("webgl context is not avaliable !"); return null;}
+        this.ctx.clearColor(1.0,1.0,1.0,1.0);
+    }
+
+    clear(){
+        this.ctx.clear(this.ctx.COLOR_BUFFER_BIT | this.ctx.DEPTH_BUFFER_BIT);
+    }
+
+    setSize(w, h){
+        this.ctx.canvas.style.width = w+"px";
+        this.ctx.canvas.style.height = h+"px";
+        this.ctx.canvas.width = w;
+        this.ctx.canvas.height = h;
+        this.ctx.viewport(0,0,w,h);
+    }
+
+    setClearColor(c){
+        this.ctx.clearColor(c.r,c.g,c.b,c.a);
+    }
+
+}
