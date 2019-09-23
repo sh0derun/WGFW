a4d2a5f7d28fb61c3915021c08c01a16aac0ad17
razikanass <razik.anas@gmail.com>
2019-09-23 18:26:08 +0200

    adding directional light

--- shaders/light.glsl
+++ shaders/light.glsl
@@ -7,3 +7,24 @@ struct PointLight {
     float linear;
     float quadratic;
 };
+
+struct DirLight {
+    vec3 direction;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+vec3 CalcDirLight(DirLight light, vec3 normal, vec3 viewDir, Material material){
+    vec3 lightDir = normalize(-light.direction);
+    // diffuse shading
+    float diff = max(dot(normal, lightDir), 0.0);
+    // specular shading
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    // combine results
+    vec3 ambient  = light.ambient  * material.ambient;
+    vec3 diffuse  = light.diffuse  * diff * material.diffuse;
+    vec3 specular = light.specular * spec * material.specular;
+    return (ambient + diffuse + specular);
+}
--- shaders/shadertemplate.glsl
+++ shaders/shadertemplate.glsl
@@ -33,26 +33,82 @@ void main( void ){
    
     vec3 col = vec3(0.0);
     
-    
     if(t.x < 20.0){
         PointLight lights[NUM_LIGHTS];
         lights[0] = PointLight(vec3(2.0,2.0,0.0),vec3(0.4,0.4,0.4),vec3(0.5,0.5,0.5),vec3(0.2,0.2,0.2),1.0,0.0014,0.000007);
         lights[1] = PointLight(vec3(0.0,5.0,-3.0),vec3(0.3,0.3,0.3),vec3(0.4,0.4,0.4),vec3(0.3,0.3,0.3),1.0,0.0014,0.000007);
         //lights[2] = PointLight(vec3(3.0,4.0,3.0),vec3(0.1,0.1,0.1),vec3(0.8,0.8,0.8),vec3(0.6,0.6,0.6),1.0,0.0014,0.000007);
 
-        for(int i = 0; i < NUM_LIGHTS; i++){
+        DirLight dirlight = DirLight(vec3(-1.0,-1.0,1.0),vec3(0.4,0.4,0.4),vec3(1.0,1.0,1.0),vec3(0.2,0.2,0.2));
+
+        if(true){
+            for(int i = 0; i < NUM_LIGHTS; i++){
+                vec3 p = camPos+t.x*d;
+                vec3 nr = normal(p);
+                vec3 light_dir = normalize(lights[i].position - p);
+                float light_shadow = smoothstep(march(p+nr*0.001, light_dir).x,0.0,1.0);
+
+                float diff = clamp(dot(nr,light_dir),0.0,1.0);
+
+                vec3 viewDir = normalize(camPos - p);
+                vec3 reflectDir = reflect(-light_dir, nr);
+                float spec = 0.0;
+                
+                vec3 z = vec3(0.0);
+                Material material;
+
+                if(t.y == 1.0){
+                    float f = smoothstep(0.2,0.1,sin(18.0*p.x)+sin(18.0*p.z)+sin(18.0*p.y));
+                    material = bronze;
+                    material.diffuse *= f;
+                    material.shininess *= 100.0;
+                }
+                else if(t.y == 3.0){
+                    material = gold;
+                    material.shininess = 120.0;
+                }
+                else if(t.y == 2.0){
+                    material = green_rubber;
+                    material.shininess *= 10.0;
+                }
+                else if(t.y == 4.0){
+                    float f = smoothstep(0.2,0.1,sin(18.0*p.x)+sin(18.0*p.z)+sin(18.0*p.y));
+                    material = green_rubber;
+                    material.diffuse *= f;
+                    material.shininess *= 10.0;
+                }
+                
+                vec3 ambient = lights[i].ambient * material.ambient;
+                vec3 diffuse = lights[i].diffuse * (diff * material.diffuse) * softshadow(p,light_dir,0.01, 2.0, 32.0);
+                
+    			diffuse = pow(diffuse, vec3(gamma));
+                
+                if(true){
+                    vec3 halfwayDir = normalize(light_dir + viewDir);
+                    spec = pow(max(dot(nr, halfwayDir), 0.0), material.shininess);
+                }
+                else{
+                    vec3 reflectDir = reflect(-light_dir, nr);
+                    spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+                }
+                
+                vec3 specular = lights[i].specular * (spec * material.specular);
+                
+                float dist = length(lights[i].position - vec3(uv,0.0));
+    			float attenuation = 1.0 / (lights[i].constant + lights[i].linear * dist + lights[i].quadratic * (dist * dist));
+                
+                ambient *= attenuation;
+                diffuse *= attenuation;
+                specular *= attenuation;
+                
+                col += ambient + diffuse + specular;
+            }
+        }
+        else{
             vec3 p = camPos+t.x*d;
             vec3 nr = normal(p);
-            vec3 light_dir = normalize(lights[i].position - p);
-            float light_shadow = smoothstep(march(p+nr*0.001, light_dir).x,0.0,1.0);
-
-            float diff = clamp(dot(nr,light_dir),0.0,1.0);
-
             vec3 viewDir = normalize(camPos - p);
-            vec3 reflectDir = reflect(-light_dir, nr);
-            float spec = 0.0;
-            
-            vec3 z = vec3(0.0);
+
             Material material;
 
             if(t.y == 1.0){
@@ -75,33 +131,20 @@ void main( void ){
                 material.diffuse *= f;
                 material.shininess *= 10.0;
             }
-            
-            vec3 ambient = lights[i].ambient * material.ambient;
-            vec3 diffuse = lights[i].diffuse * (diff * material.diffuse) * softshadow(p,light_dir,0.01, 2.0, 32.0);
-            
-			diffuse = pow(diffuse, vec3(gamma));
-            
-            if(true){
-                vec3 halfwayDir = normalize(light_dir + viewDir);
-                spec = pow(max(dot(nr, halfwayDir), 0.0), material.shininess);
-            }
-            else{
-                vec3 reflectDir = reflect(-light_dir, nr);
-                spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-            }
-            
-            vec3 specular = lights[i].specular * (spec * material.specular);
-            
-            float dist = length(lights[i].position - vec3(uv,0.0));
-			float attenuation = 1.0 / (lights[i].constant + lights[i].linear * dist + lights[i].quadratic * (dist * dist));
-            
-            ambient *= attenuation;
-            diffuse *= attenuation;
-            specular *= attenuation;
+
+            vec3 lightDir = normalize(-dirlight.direction);
+            // diffuse shading
+            float diff = max(dot(nr, lightDir), 0.0);
+            // specular shading
+            vec3 reflectDir = reflect(-lightDir, nr);
+            float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+            // combine results
+            vec3 ambient  = dirlight.ambient  * material.ambient;
+            vec3 diffuse  = dirlight.diffuse  * diff * material.diffuse * softshadow(p,lightDir,0.01, 2.0, 32.0);
+            vec3 specular = dirlight.specular * spec * material.specular;
             
             col += ambient + diffuse + specular;
         }
-        
     }
 
     vec3 skyCol = fogColor - d.y;
